be841c16cb24962d8d92feede0c95215ef7c307c
Sven A. Schmidt <sas@finestructure.co>
2020-03-02 14:13:51 +0100

    Fix dependency ref

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ArenaTest",
-            dependencies: ["RxSwift", "RxCocoa"]),
+            dependencies: ["Parser"]),
         .testTarget(
             name: "ArenaTestTests",
             dependencies: ["ArenaTest"]),
